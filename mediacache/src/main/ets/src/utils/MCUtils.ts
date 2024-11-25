8f06a03e15e3cd7864cce0506fcf72edf4a72b1d
changsanjiang <changsanjiang@gmail.com>
2024-11-25 11:03:42 +0800

    update download

--- mediacache/src/main/ets/src/utils/MCUtils.ts
+++ mediacache/src/main/ets/src/utils/MCUtils.ts
@@ -8,6 +8,26 @@ namespace MCUtils {
     });
     return record;
   }
+
+  // Map转为Record
+  export function mapToRecord(map: Map<string, string>): Record<string, string> {
+    return Object.fromEntries(map.entries()) as Record<string, string>;
+  }
+
+  // Record转为Map
+  export function recordToMap(record: Record<string, string>): Map<string, string> {
+    let myMap: Map<string, string> = new Map();
+    for (const key in record) {
+      myMap.set(key, record[key]);
+    }
+    return myMap;
+  }
+
+  export function modifyRecord(record: Record<string, string>, key: string, value?: string): Record<string, string> {
+    let map = recordToMap(record);
+    value != undefined ? map.set(key, value) : map.delete(key);
+    return mapToRecord(map);
+  }
 }
 
 export default MCUtils;
