60d603fd720c8e1240afc70747361d55add4678e
changsanjiang <changsanjiang@gmail.com>
2024-11-28 21:31:01 +0800

    update parser

--- mediacache/src/main/ets/src/utils/MCUtils.ts
+++ mediacache/src/main/ets/src/utils/MCUtils.ts
@@ -38,6 +38,20 @@ namespace MCUtils {
     const blob = await md.digest();
     return blob.data.slice(0, 16).reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
   }
+
+  export function replaceCharactersInRange(str: string, range: [number, number], replacement: string): string {
+    const [start, length] = range;
+
+    // 检查范围是否合法
+    if (start < 0 || length < 0 || start + length > str.length) {
+      throw new Error("Invalid range");
+    }
+
+    // 截取字符串并拼接替换内容
+    const before = str.slice(0, start); // 替换前部分
+    const after = str.slice(start + length); // 替换后部分
+    return before + replacement + after;
+  }
 }
 
 export default MCUtils;
